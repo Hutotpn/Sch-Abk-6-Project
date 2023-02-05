fc266a55d83fa10cbb66dafdae13c7d036673ef2
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-02-05 21:32:47 +0700

    Update pw-gen.py

--- Python files/pw-gen.py
+++ Python files/pw-gen.py
@@ -3,40 +3,36 @@ import string
 import time
 
 def generate_password(length, use_uppercase=True, use_numbers=True, use_special_characters=True):
-    # Create a list of characters to use in the password
     characters = string.ascii_lowercase
-    
     if use_uppercase:
         characters += string.ascii_uppercase
     if use_numbers:
         characters += string.digits
     if use_special_characters:
         characters += string.punctuation
-    
-    # Use the random module to shuffle the characters
     password = ''.join(random.sample(characters, length))
     return password
 
-# Welcome message
 print("Welcome to the Random Password Generator!")
-
 time.sleep(1)
 
 while True:
-    # Prompt the user for the password length
-    length = int(input("How many characters would you like your password to have? "))
+    length = int(input("Enter the desired length of your password (minimum 6 characters): "))
+    if length < 6:
+        print("For your safety, password length must be at least 6 characters. Please try again.")
+        continue
 
-    # Prompt the user for the type of characters to use
-    use_uppercase = input("Use uppercase letters? (y/n) ") == 'y'
-    use_numbers = input("Use numbers? (y/n) ") == 'y'
-    use_special_characters = input("Use special characters? (y/n) ") == 'y'
+    print("Choose the types of characters to include in your password:")
+    time.sleep(1)
+    use_uppercase = input("Include uppercase letters? (y/n) ").lower() == 'y'
+    use_numbers = input("Include numbers? (y/n) ").lower() == 'y'
+    use_special_characters = input("Include special characters? (y/n) ").lower() == 'y'
 
-    # Generate the password
     password = generate_password(length, use_uppercase, use_numbers, use_special_characters)
-    time.sleep(2)
-    print("Your generated password is: " + password)
+    print(f"Your generated password is: {password}")
 
-    try_again = input("Do you want to generate another password? (y/n) ")
-    if try_again.lower() != 'y':
+    try_again = input("Do you want to generate another password? (y/n) ").lower()
+    if try_again != 'y':
         print("Thank you for using the Random Password Generator. Goodbye!")
+        time.sleep(3)
         break
