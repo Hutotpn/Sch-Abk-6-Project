0c05bdd77613ded82f033c595d357e2bd93fdade
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-02-05 21:19:43 +0700

    Update num-gen.py

--- Python files/num-gen.py
+++ Python files/num-gen.py
@@ -1,35 +1,26 @@
 import random
 from time import sleep
 
-def grn():
+def generate_random_number():
   # Prompt the user to enter the lower and upper bounds of the range
-  low = int(input("Enter the lower bound of the range: "))
-  high = int(input("Enter the upper bound of the range: "))
+  lower_bound = int(input("Enter the lower bound of the range: "))
+  upper_bound = int(input("Enter the upper bound of the range: "))
 
   # Generate a random integer between low and high (inclusive)
-  return random.randint(low, high)
+  return random.randint(lower_bound, upper_bound)
 
 # Welcome & Generate a random number within the user-specified range
-print("Welcome to the Number Generator!")
+print("Welcome to the Random Number Generator!")
 
 sleep(1)
 
-while True:
-  print(grn())
-  
-  # Try again?
-  sleep(1)
-  print("Do you want to try again?: ")
-  # Ask for input
-  tryagain = input("Enter y/n: ")
-  # If yes, do it again
-  if tryagain == 'y':
-    print("Reloading!")
-    sleep(0.5)
-    print("Reloaded!")
- 
-# If no, exit
-else:
-  print("Goodbye!")
-  sleep(3)
-  break
+try_again = True
+while try_again:
+  print(generate_random_number())
+
+  # Ask the user if they want to try again
+  try_again = input("Do you want to generate another random number? (y/n) ").lower() == 'y'
+
+# Goodbye message
+print("Goodbye!")
+sleep(3)
