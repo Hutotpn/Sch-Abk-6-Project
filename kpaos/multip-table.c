bc5b5bacc99bf6628d70294ae527244b7016c478
Huto <92351427+Hutotpn@users.noreply.github.com>
2024-02-15 12:10:28 +0700

    Create multip-table.c

---
+++ kpaos/multip-table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // Variable
+    int a,b;
+    
+    // Input
+    printf("Input number: ");
+    scanf("%d", &a);
+    
+    // For loop
+    for(b=1; b <= 12; b++) {
+        printf("%d x %d = %d\n", a, b, a*b)
+    }
+    
+    return 0;
+}
